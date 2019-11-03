bb82c40bd1975f036cba2914178763cc2ed94e08
pwlae <pavel.danelian@gmail.com>
2019-11-03 19:45:49 +0100

    sqrt error handling added

--- tour-of-go/sqrt.go
+++ tour-of-go/sqrt.go
@@ -4,15 +4,33 @@ import (
 	"fmt"
 )
 
-func Sqrt(x float64) float64 {
-	z := x/4
-	for i := 0; x - z * z < 1 / 10; i++ {
-		print("step ", i, "\n")
-		z -= (z*z - x) / (2*z)
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %s", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
 	}
-	return z
+
+	z := x / 4
+
+	// In use to debug number of steps
+	//for i := 0; x - z * z < 1 / 10; i++ {
+	for x-z*z < 1/10 {
+
+		// In use to debug number of steps
+		// print("step ", i, "\n")
+		z -= (z*z - x) / (2 * z)
+	}
+
+	return z, nil
 }
 
 func main() {
-	fmt.Println(Sqrt(123847238946))
+	fmt.Println(Sqrt(15.0))
+	fmt.Println(Sqrt(-15.0))
+
 }
